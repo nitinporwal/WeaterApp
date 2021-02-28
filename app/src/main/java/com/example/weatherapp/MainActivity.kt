30c926ee02c59bd704005724819115f8028186ed
nitin-codr <nitinporwal99@gmail.com>
2021-02-28 17:03:23 +0530

    add permissions for location using dexter

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.weatherapp
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var mFusedLocationClient: FusedLocationProviderClient
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+    if (!isLocationEnabled()) {
+      Toast.makeText(
+        this,
+        "Your location provider is turned off. Please turn it on.",
+        Toast.LENGTH_SHORT
+      ).show()
+      val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+      startActivity(intent)
+    } else {
+      Dexter.withActivity(this).withPermissions(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+      ).withListener(object : MultiplePermissionsListener {
+        override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+          if (report!!.areAllPermissionsGranted()) {
+            requestLocationData()
+          }
+
+          if (report.isAnyPermissionPermanentlyDenied) {
+            Toast.makeText(
+              this@MainActivity,
+              "You had denied location permission. Please enable them as it is mandatory for the app to work.",
+              Toast.LENGTH_SHORT
+            ).show()
+          }
+        }
+
+        override fun onPermissionRationaleShouldBeShown(
+          permissions: MutableList<PermissionRequest>?,
+          token: PermissionToken?
+        ) {
+          showRationalDialogForPermissions()
+        }
+      }).onSameThread().check()
+    }
+  }
+
+  @SuppressLint("MissingPermission")
+  private fun requestLocationData() {
+    val mLocationRequest = LocationRequest()
+    mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+    mFusedLocationClient.requestLocationUpdates(
+      mLocationRequest, mLocationCallback, Looper.myLooper()
+    )
+  }
+
+  private val mLocationCallback = object : LocationCallback() {
+    override fun onLocationResult(locationResult: LocationResult) {
+      val mLastLocation: Location = locationResult.lastLocation
+      val latitude = mLastLocation.latitude
+      val longitude = mLastLocation.longitude
+      Log.i("Current Latitude", "$latitude")
+      Log.i("Current Longitude", "$longitude")
+    }
+  }
+
+  public fun showRationalDialogForPermissions() {
+    AlertDialog.Builder(this)
+      .setMessage("It looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+      .setPositiveButton("GO TO SETTINGS") { _, _ ->
+        try {
+          val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+          val uri = Uri.fromParts("package", packageName, null)
+          intent.data = uri
+          startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+          e.printStackTrace()
+        }
+      }.setNegativeButton("Cancel") { dialog, _ -> dialog.dismiss() }.show()
+  }
+
+  private fun isLocationEnabled(): Boolean {
+    val locationManager: LocationManager =
+      getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+      LocationManager.NETWORK_PROVIDER
+    )
+  }
+}
